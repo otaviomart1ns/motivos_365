6a1749199627bd7fef7f74290fc93d43896d2495
otaviomart1ns <otavio.martins01@hotmail.com>
2023-05-11 02:30:26 -0300

    feat: Adicionando recursos html, css a tela de index.

--- motivos/models.py
+++ motivos/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 class Bilhete(models.Model):
-    mensagem = models.TextField()
-    data = models.DateTimeField(auto_now_add=True)
+    message = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.mensagem
+        return self.message
--- motivos/views.py
+++ motivos/views.py
@@ -1,20 +1,18 @@
-from django.contrib.auth import authenticate, login
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from motivos.models import Bilhete
+
 
 def login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect('/')
-        else:
-            error = "Usuário ou senha incorretos"
-            return render(request, 'registration/login.html', {'error': error})
-    else:
-        return render(request, 'registration/login.html')
+    return render(request, 'registration/login.html')
     
 def index(request):
-        return render(request, 'motivos/index.html')
+    context = {}
+    if not request.user.last_login:
+        try:
+            msg = Bilhete.objects.get(pk=1)
+            context = {'msg': msg.message}
+        except Bilhete.DoesNotExist:
+            pass
+        
+
+    return render(request, 'motivos/index.html', context)
